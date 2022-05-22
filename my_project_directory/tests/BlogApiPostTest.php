94823372d389eda4b42c3f8bf61d2df86589f06d
Al <70017631+albert-all@users.noreply.github.com>
2022-05-22 23:46:27 +0300

    Добавление testApi

--- my_project_directory/tests/BlogApiPostTest.php
+++ my_project_directory/tests/BlogApiPostTest.php
@@ -6,11 +6,65 @@ use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 
 class BlogApiPostTest extends ApiTestCase
 {
-    public function testSomething(): void
+    public function apiToken(): string
     {
-        $response = static::createClient()->request('GET', '/');
-
-        $this->assertResponseIsSuccessful();
-        $this->assertJsonContains(['@id' => '/']);
+        $user = static::getContainer()->get(UserRepository::class)->findOneBy(['	email' => 'albert.e.m@mail.ru']);
+        return $user->getApiToken();
     }
+
+//    public function testApiUsers()
+//    {
+//        $client = static::createClient();
+//        $client->request('GET', '/api/users');
+//        $this->assertResponseStatusCodeSame(401);
+//        $response = $client->withOptions([
+//            'headers' => ['x-auth-token' => $this->apiToken(), 'content-type' => 'application/json; charset=utf-8'],
+//        ])->request('GET', '/api/users');
+//        $this->assertResponseStatusCodeSame(200);
+//        $resultArray = $response->toArray();
+//        $this->assertJson($response->getContent());
+//        $this->assertIsArray($resultArray);
+//    }
+//
+//    public function testQuestions(): void
+//    {
+//        $client = static::createClient();
+//        $client->request('GET', '/api/questions');
+//        $this->assertResponseStatusCodeSame(401);
+//        $response = $client->withOptions([
+//            'headers' => ['x-auth-token' => $this->apiToken(), 'content-type' => 'application/json; charset=utf-8'],
+//        ])->request('GET', '/api/questions');
+//        $this->assertResponseStatusCodeSame(200);
+//        $resultArray = $response->toArray();
+//        $this->assertJson($response->getContent());
+//        $this->assertIsArray($resultArray);
+//    }
+//
+//    public function testAnswers(): void
+//    {
+//        $client = static::createClient();
+//        $client->request('GET', '/api/aswers');
+//        $this->assertResponseStatusCodeSame(401);
+//        $response = $client->withOptions([
+//            'headers' => ['x-auth-token' => $this->apiToken(), 'content-type' => 'application/json; charset=utf-8'],
+//        ])->request('GET', '/api/aswers');
+//        $this->assertResponseStatusCodeSame(200);
+//        $resultArray = $response->toArray();
+//        $this->assertJson($response->getContent());
+//        $this->assertIsArray($resultArray);
+//    }
+//
+//    public function testCategories(): void
+//    {
+//        $client = static::createClient();
+//        $client->request('GET', '/api/categories');
+//        $this->assertResponseStatusCodeSame(401);
+//        $response = $client->withOptions([
+//            'headers' => ['x-auth-token' => $this->apiToken(), 'content-type' => 'application/json; charset=utf-8'],
+//        ])->request('GET', '/api/categories');
+//        $this->assertResponseStatusCodeSame(200);
+//        $resultArray = $response->toArray();
+//        $this->assertJson($response->getContent());
+//        $this->assertIsArray($resultArray);
+//    }
 }
